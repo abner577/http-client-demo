d4ded18d986f43fa3535b54253975b97e29d2ede
Abner Rodriguez <abner07282005@gmail.com>
2026-01-14 11:50:32 -0500

    GET all endpoint

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,13 @@
+import todo.TodoClient;
+
+import java.io.IOException;
+
 public class Application {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        var todoClient = new TodoClient();
+        var response = todoClient.getAll();
+        System.out.println(response);
     }
 }
--- src/main/java/todo/TodoClient.java
+++ src/main/java/todo/TodoClient.java
@@ -1,5 +1,32 @@
 package todo;
 
-// Class where we are going to be talking to the JSONplaceholder API
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+/**
+ Class where we are going to be talking to the JSONplaceholder API
+ */
 public class TodoClient {
+
+    private final String BASE_API_URL = "https://jsonplaceholder.typicode.com/todos";
+    private final HttpClient client;
+
+    public TodoClient() {
+        client = HttpClient.newHttpClient();
+    }
+
+
+    public String getAll() throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(BASE_API_URL))
+                .GET()
+                .build();
+
+        HttpResponse<String> response  = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        return response.body();
+    }
 }
